2176a46ad057798f642f110a29053dd65fa7cdd3
shagohead <html.ru@gmail.com>
2024-01-10 12:09:18 +0300

    Merge secsqc tests & upgrade ogen to 0.78.0

--- secsrc/secsrc_test.go
+++ secsrc/secsrc_test.go
@@ -9,57 +9,8 @@ import (
 	"github.com/kruzhok-team/berloga-go/berauth"
 )
 
-func TestContextSource(t *testing.T) {
+func TestSecuritySources(t *testing.T) {
 	ctx := context.Background()
-	for _, tt := range []struct {
-		name    string
-		ctx     context.Context
-		bjwtTok string
-		bjwtErr error
-		skeyTok string
-		skeyErr error
-	}{
-		{
-			name:    "UnauthCtx",
-			ctx:     ctx,
-			bjwtErr: ogenerrors.ErrSkipClientSecurity,
-			skeyErr: ogenerrors.ErrSkipClientSecurity,
-		},
-		{
-			name:    "BerlogaJWT",
-			ctx:     berauth.SetPlayer(ctx, berauth.Player{Token: "playerToken"}),
-			bjwtTok: "playerToken",
-			skeyErr: ogenerrors.ErrSkipClientSecurity,
-		},
-		{
-			name:    "ServiceKey",
-			ctx:     berauth.SetServiceKey(ctx, "serviceKey"),
-			bjwtErr: ogenerrors.ErrSkipClientSecurity,
-			skeyTok: "serviceKey",
-		},
-	} {
-		if tt.ctx == nil {
-			tt.ctx = context.Background()
-		}
-		src := Context()
-		tok, err := src.BerlogaJWT(tt.ctx, "testOp")
-		if tok != tt.bjwtTok {
-			t.Errorf("BerlogaJWT токен содержит `%s`, ожидалось `%s`", tok, tt.bjwtTok)
-		}
-		if err != tt.bjwtErr {
-			t.Errorf("BerlogaJWT ошибка содержит `%v`, ожидалось `%v`", tok, tt.bjwtTok)
-		}
-		tok, err = src.ServiceKey(tt.ctx, "testOp")
-		if tok != tt.skeyTok {
-			t.Errorf("ServiceKey токен содержит `%s`, ожидалось `%s`", tok, tt.skeyTok)
-		}
-		if err != tt.skeyErr {
-			t.Errorf("ServiceKey ошибка содержит `%v`, ожидалось `%v`", tok, tt.skeyTok)
-		}
-	}
-}
-
-func TestTokenSource(t *testing.T) {
 	for _, tt := range []struct {
 		name    string
 		src     SecuritySource
@@ -69,6 +20,27 @@ func TestTokenSource(t *testing.T) {
 		skeyTok string
 		skeyErr error
 	}{
+		{
+			name:    "UnauthCtx",
+			src:     Context(),
+			ctx:     ctx,
+			bjwtErr: ogenerrors.ErrSkipClientSecurity,
+			skeyErr: ogenerrors.ErrSkipClientSecurity,
+		},
+		{
+			name:    "BerlogaJWT",
+			src:     Context(),
+			ctx:     berauth.SetPlayer(ctx, berauth.Player{Token: "playerToken"}),
+			bjwtTok: "playerToken",
+			skeyErr: ogenerrors.ErrSkipClientSecurity,
+		},
+		{
+			name:    "ServiceKey",
+			src:     Context(),
+			ctx:     berauth.SetServiceKey(ctx, "serviceKey"),
+			bjwtErr: ogenerrors.ErrSkipClientSecurity,
+			skeyTok: "serviceKey",
+		},
 		{
 			name:    "BerlogaJWT",
 			src:     BerlogaJWT("testToken"),
